78f494d464fac3c30e8eaca1cdfa73398446fd88
Freddy <fredericdouamba2014@gmail.com>
2023-10-16 16:53:27 -0700

    Initial Commit

---
+++ game/gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameApp.swift
+//  game
+//
+//  Created by Student Account on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
